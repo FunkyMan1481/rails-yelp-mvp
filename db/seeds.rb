bb179b6c7504a349d9f926ce1398eceb34dc15d4
stanislas zablocki <stanislas.zablocki@gmail.com>
2023-05-24 15:56:23 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.destroy_all
+Restaurant.create!(name: 'Resto de Chacal', address: '18 Rue Chacal', category: 'chinese', phone_number:"0647487548")
+Restaurant.create!(name: 'Chacalus Maximus', address: '18 One Two Three', category: 'french', phone_number:"0647487548")
+Restaurant.create!(name: 'Marabout', address: '18 Rue Maraboutage', category: 'italian', phone_number:"0647487548")
+Restaurant.create!(name: 'Végéta', address: '18 Rue Saiyan', category: 'japanese', phone_number:"0647487548")
+Restaurant.create!(name: 'Arsenal', address: '18 Rue on a perdu le titre', category: 'chinese', phone_number:"0647487548")
